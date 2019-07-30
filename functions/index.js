569064687bf4c518d403d7c0da7bf311201a897b
Henry Oliver <henry@7wlite.com>
2019-07-30 07:28:11 -0400

    feat(markup): finish semantic markup

---
+++ functions/index.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true });
+const functions = require('firebase-functions');
+
+// Load environment variables
+require('dotenv').config();
+
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true, // use TLS
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+exports.email = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        const { name, email, phone, message } = request.body;
+
+        transporter.sendMail({
+            from: '"Ralph Brinker" <contact@Ralph Brinker.com>',
+            to: 'contact@Ralph Brinker.com',
+            subject: 'Contact us',
+            html: `Contact from the website:</br></br>
+                Name: <em>${ name }</em></br></br>
+                Email: <em>${ email }</em></br></br>
+                Phone: <em>${ phone }</em></br></br>
+                Message: <em>${ message }`
+        }, (error, info) => {
+            if (error) {
+                console.error(error);
+                return response.status(400).end();
+            }
+
+            console.info(info);
+            return response.status(200).end();
+        });
+    });
+});
---
+++ site/scripts/main.js
@@ -0,0 +1,72 @@
+(function() {
+    // Scroll to element
+    document.querySelectorAll('.js-scroll').forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const target = element.href.split('#').slice(-1).pop();
+
+            document.querySelector(`[name='${ target }']`).scrollIntoView({ behavior: 'smooth' });
+        }, false);
+    });
+
+    // Send form submit
+    const form = document.querySelector('.js-form');
+    const formTitle = document.querySelector('.js-title');
+    const messageThanks = document.querySelector('.js-thanks');
+    const messageError = document.querySelector('.js-error');
+    const sendButton = document.querySelector('.js-send');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+
+        let data = {};
+
+        for (let [key, value] of formData.entries()) {
+            data[key] = value;
+        }
+
+        sendButton.classList.toggle('button--disable');
+        messageError.classList.replace('visible', 'hidden');
+        messageThanks.classList.replace('visible', 'hidden');
+
+        if (grecaptcha.getResponse()) {
+            send(data);
+        } else {
+            sendButton.classList.toggle('button--disable');
+            messageError.classList.replace('hidden', 'visible');
+        }
+    });
+
+    async function send(data) {
+        const headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+
+        try {
+            const response = await fetch('https://us-central1-sevenwlite-project.cloudfunctions.net/email', {
+                method: 'POST',
+                headers,
+                mode: 'cors',
+                cache: 'default',
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+
+            form.classList.replace('opaque', 'transparent');
+            formTitle.classList.replace('opaque', 'transparent');
+            messageThanks.classList.replace('hidden', 'visible');
+        } catch (error) {
+            console.error(error);
+            sendButton.classList.toggle('button--disable');
+            messageError.classList.replace('hidden', 'visible');
+        } finally {
+            console.info('Finally');
+        }
+    }
+})();
